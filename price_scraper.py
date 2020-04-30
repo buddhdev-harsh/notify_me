1e9065833b1e6a78f8bde2e9c77d4e67b3223854
pruthvi03 <hingu.bloggers@gmail.com>
2020-04-30 10:57:04 +0530

    updated01

--- price_scraper.py
+++ price_scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import json
+import re
 ##############################################
 
 class scrapit():
@@ -9,34 +11,20 @@ class scrapit():
         self.query = query
         self.price = price
 
-    def query_search_scrap(self):
-        headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
-
-        req = requests.get(self.query,headers = headers)
-        soup = BeautifulSoup(req.content , 'html.parser')
-        
-        outer_table = soup.find("h1",{"class":"pdp-title"}).text
-        print(outer_table)
-        #edit the above code for price extraction on myntra
-        price = float(price[2:5])
-        print(price)
-        print(title)
-        if(price < self.price):
-            send_mail()
-        else:
-            print('no mail')
-
-    def send_mail():
+    def send_mail(self,product_body):
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
 
         server.login('harahismast@gmail.com', 'harshzxcvbnmlpoiuytrewqaz')
-        subject = 'price fell down'
+        subject = 'price fell down\n'
         body = self.query
-        msg = "Subject: "+subject+"\n "+body
+
+        # product_body = 'price'+product_dict['price']+'brand_name'+product_dict['brand_name']+'product_name'+ product_dict['product_name']
+
+        msg = "Subject: "+subject+"\n "+body+"\n"+product_body
+        print(msg)
 
         server.sendmail(
             'harahismast@gmail.com',
@@ -45,6 +33,41 @@ class scrapit():
         )
         print('HEY EMAIL HAS BEEN SENT')
         server.quit()
+
+
+    def query_search_scrap(self):
+        headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+
+        req = requests.get(self.query,headers = headers)
+        soup = BeautifulSoup(req.content , 'html.parser')
+        
+        script_contents = soup.find_all('script')
+        product_script = script_contents[1]
+        product_script = product_script.text.replace(" ","").replace("\n","").replace("\t","")
+
+        product_json = json.loads(product_script)
+
+        price = int(product_json['offers']['price'])
+        brand_name = product_json['brand']['name']
+        product_name = product_json['name'][len(brand_name):]
+
+        product_name = " ".join(re.findall('[A-Z][^A-Z]*',product_name))
+
+        product_dict = {
+            'price':price,
+            'brand_name':brand_name,
+            'product_name': product_name
+        }
+
+        product_body = 'price : '+str(price)+'\nbrand_name : '+brand_name+'\nproduct_name : '+ product_name
+        print(product_body)
+        print("\n\n",type(price)," ",type(self.price) ,"\n\n")
+        if(price < self.price):
+            self.send_mail(product_body)
+        else:
+            print('no mail')
+
             
 
 
